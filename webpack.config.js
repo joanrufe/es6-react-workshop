37150d138a64d8048f330729d0f6cbae5dfaeeb1
Antonio Ruiz <stayforawhile@gmail.com>
2017-10-24 00:00:29 +0200

    Webpack dev server and hot loader

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var basePath = __dirname;
+
+module.exports = {
+    context: basePath,
+    entry: {
+        bundle: [
+            './src/app.js'
+        ]
+    },
+    output: {
+        path: path.join(basePath,'dist'),
+        filename: 'app.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        hot:true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html', // Name of template in ./src
+            hash: true,
+            cache: false
+          }),
+        new webpack.HotModuleReplacementPlugin(),
+    ]
+}
